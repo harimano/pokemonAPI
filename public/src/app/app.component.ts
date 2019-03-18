77e34d028c4cf6bbfa8f74cb7708c2ab706e2067
Hari Prasath Manokaran <38764929+hari5719@users.noreply.github.com>
2019-03-18 16:21:13 -0700

    pinging api

---
+++ public/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HttpService } from './http.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pokeinfo :any;
+
+constructor(private _httpService: HttpService ){
+    
+  _httpService.getPokemonbulb().subscribe(data =>
+    {
+      this.pokeinfo = data;
+      console.log(data);
+    });
+}
+
+
+
+}
+
